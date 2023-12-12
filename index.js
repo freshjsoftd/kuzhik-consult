ed0a8fd8a201957725e2ba932c31f27d6c9d59cb
yan <yan.braverman37@gmail.com>
2023-12-12 19:17:45 +0200

    After consult #2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let user = {
-	fullName: null,
+	fullName: 'st',
 	age: 20,
 	isMarried: true,
 };
@@ -9,7 +9,7 @@ const array = [1, 2];
 function sum(a, b) {
 	return a + b;
 }
-console.log(user);
+// console.log(user);
 /* console.log(typeof user.age)
 console.log(typeof user.fullName);
 console.log(typeof user.isMarried);
@@ -25,17 +25,88 @@ console.log(typeof array); */
 	console.log('It is a boolean');
 } */
 
-const key = typeof user.fullName;
+/* const key = 3;
 
 switch (key) {
-	case 'string':
-		console.log('It is a string');
+	case 0:
+		console.log('Level0');
 		break;
-	case 'number':
-		console.log('It is a number');
+	case 1:
+		console.log('Level1');
 		break;
-	case 'boolean':
-		console.log('It is a boolean');
+	case 2:
+		console.log('Level2');
+		break;
+	case 3:
+		console.log('Level3');
+		break;
+	case 4:
+		console.log('Level4');
+		break;
+	case 5:
+		console.log('Level5');
+		break;
+	case 6:
+		console.log('Level6');
+		break;
+	case 7:
+		console.log('Level7');
 		break;
   default: console.log('I dont know');
+} */
+
+// key === 'string' ? console.log('It is a string') : console.log('It is not a string');
+
+// Loop
+let i = 1;
+/* while(count < 10){
+	console.log('Count = ', count)
+	count++;
+} */
+
+/* do {
+	console.log('Count = ', count);
+	count++;
+} while (count < 10); */
+
+
+/* for(; i < 10; i++){
+	let sq = i ** 2;
+	if(i === 5) continue;
+	console.log('Sq = ', sq);
+	if(i === 7) break;
+} */
+
+// Finction
+
+/* let a = 30;
+let b = 20;
+
+let c = a + b;
+
+// console.log(c)
+
+c = summ(10, 5)
+
+function summ(a, b) {
+	return a + b;
+	// console.log()
+}
+
+
+
+console.log(c)
+
+const add = function (a, b) {
+	return a + b;
+};
+let d = add(20, 40)
+
+console.log(d); */
+SimpleNumbers:
+for(let i = 2; i <= 100; i++){
+	for(let j = 2; j < i; j++){
+		if(i % j === 0) continue SimpleNumbers;
+	}
+	console.log(i)
 }
