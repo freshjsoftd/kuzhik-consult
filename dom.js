b072c7bafddae6ed1269fa7bb571ef7ab993ab7f
yan <yan.braverman37@gmail.com>
2024-01-16 17:47:46 +0200

    End consult #7

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-const nav = document.getElementById('nav1');
+/* const nav = document.getElementById('nav1');
 const tag = document.getElementsByTagName('li');
 const selector = document.querySelector('li');
 
@@ -11,9 +11,9 @@ console.log(tag);
 console.log(selector);
 
 nav.append(selector);
-nav.append(article);
+nav.append(article); */
 
-const promise = new Promise((resolve, reject) => {
+/* const promise = new Promise((resolve, reject) => {
     setTimeout(() => resolve('Is done'), 2000)
 })
 .then((done) => {
@@ -24,8 +24,41 @@ const promise = new Promise((resolve, reject) => {
     console.log(err)
     return err;
 })
+console.log(promise); */
 
+// async/await
 
+async function summ(a, b) {
+	try {
+		const result = a + b;
+		const value = await Promise.resolve(result + ' $');
+		console.dir(value);
+		const final = (await value) + ' end';
+		console.log(final);
+		return final;
+	} catch (error) {
+		console.log(error);
+	} finally {
+		console.log('This code will run anyway');
+	}
+}
+summ(10, 20);
 
-console.log(promise);
+// AJAX
+
+const getUsers = async () => {
+	try {
+		const data = await fetch(`https://jsonplaceholder.typicode.com/users`);
+		console.log(data);
+		const users = data.json();
+		console.log(users);
+		return users;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+getUsers();
+
+// Weather
 
